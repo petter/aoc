456c35b7aceb05b51aba92e2ba9becb6c47b8261
pettersmoen <pettersmoen@gmail.com>
2018-12-14 19:07:51 +0100

    Problem 1 day 5 solved

---
+++ day5/problem1.hs
@@ -0,0 +1,21 @@
+import Data.List
+import Data.Char
+import System.IO
+
+polymerReact :: [Char] -> [Char]
+polymerReact x
+    | newStr /= x = polymerReact newStr
+    | otherwise = newStr
+    where 
+        newStr = polymerReactCycle x
+
+polymerReactCycle :: [Char] -> [Char]
+polymerReactCycle [] = []
+polymerReactCycle (a:[]) = [a]
+polymerReactCycle (a:b:abs) 
+    | a /= b && (toLower a) == (toLower b) = polymerReactCycle abs
+    | otherwise = a : (polymerReactCycle (b:abs))
+
+main = do
+    contents <- readFile "input.txt"
+    putStrLn (show (length (polymerReact contents)))
