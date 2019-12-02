40f9aeb2bc8e53e81deba19658366aaf3e33a779
Petter Sæther Moen <pettersmoen@gmail.com>
2019-12-02 10:50:24 +0100

    Solved problem 2 day 2

--- 2019/src/day2/Day2.kt
+++ 2019/src/day2/Day2.kt
@@ -4,23 +4,36 @@ import java.io.File
 
 fun main() {
 
-    val opcodes = File("in/day2.txt").readText().trim().split(",").map { it.toInt() }.toMutableList()
-    opcodes[1] = 12
-    opcodes[2] = 2
+    val opcodes = File("in/day2.txt").readText().trim().split(",").map { it.toInt() }
+    for (noun in 0..99) {
+        for (verb in 0..99) {
+            val res = run(opcodes, noun, verb)
+            if(res == 19690720) {
+                println("Noun: $noun, Verb: $verb. 100 * noun + verb = ${100 * noun + verb}")
+                return;
+            }
+        }
+    }
+    println(opcodes)
+}
 
+
+
+fun run(input: List<Int>, noun : Int, verb : Int) : Int {
+
+    val opcodes = input.toMutableList()
+    opcodes[1] = noun
+    opcodes[2] = verb
     for (i in 0..opcodes.size step 4) {
-        println("\nindex $i")
         val opcode = opcodes[i]
-        println("opcode $opcode")
         if(opcode == 99) break
 
         val vals = opcodes.subList(i+1, i+3).map { opcodes[it] }
         val saveIndex = opcodes[i+3]
         val res = if (opcode == 1) vals.sum() else vals.reduce {acc, i -> acc * i}
-        println(vals)
-        println(res)
 
         opcodes[saveIndex] = res
     }
-    println(opcodes)
+
+    return opcodes[0]
 }
