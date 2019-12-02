4ca148a93ad25b38ae35725d153ee5b14eb6b8b7
Petter Sæther Moen <pettersmoen@gmail.com>
2019-12-02 18:55:30 +0100

    Solved problem 2 day 8 2018

--- 2018/day8/Day8.kt
+++ 2018/day8/Day8.kt
@@ -7,14 +7,14 @@ var i = 0
 fun main() {
     val numbers = File("2018/day8/input.txt").readText().trim().split(" ").map {it.toInt()}
     val root = createTree(numbers, 0)
-    println("Sum of metadata: ${root.sum()}")
+    println("1) Sum of metadata: ${root.sum()}")
+    println("2) Sum of metadata: ${root.metadataSum()}")
 }
 
 fun createTree(numbers: List<Int>, depth: Int) : Node {
     val numChildren = numbers[i++]
     val numMetadata = numbers[i++]
     val r = (0..numChildren).drop(1)
-    println("numChildren: $numChildren, numMetadata: $numMetadata, depth: $depth, range: $r")
 
     val children = r.map { createTree(numbers, depth + 1) }
     val metadata = numbers.subList(i, i + numMetadata)
@@ -25,5 +25,13 @@ fun createTree(numbers: List<Int>, depth: Int) : Node {
 
 class Node(private val children: List<Node>, private val metadata: List<Int>) {
     fun sum() : Int = metadata.sum() + children.fold(0, {acc, node -> acc + node.sum()})
+
+    fun metadataSum() : Int =
+        if(children.isEmpty()) metadata.sum()
+        else metadata.mapNotNull { children.getOrNull(it - 1) }
+                .map {it.metadataSum()}
+                .sum()
+
+
 }
 
