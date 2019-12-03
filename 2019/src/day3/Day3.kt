0ea4ebc50660f1d371757f1d0a149c549de9c2ad
Petter Sæther Moen <pettersmoen@gmail.com>
2019-12-03 13:48:04 +0100

    Solved day 3 problem 2 2019

--- 2019/src/day3/Day3.kt
+++ 2019/src/day3/Day3.kt
@@ -7,26 +7,30 @@ fun main() {
     val grid = WireGrid()
     File("in/day3.txt").readLines().forEach { grid.addWire(it) }
     println("Closest distance to intersection: ${grid.closestDistanceToIntersection()}")
+    println("Minimum steps intersections: ${grid.minimumStepsIntersection()}")
 }
 
 class WireGrid() {
-
-    val map = HashMap<String, String>()
+    val map = HashMap<String, Array<String>>()
     val intersections = mutableListOf<Array<Int>>()
+    val intersectionSteps = mutableListOf<Array<Int>>()
 
-    private companion object {
+    companion object {
         var curId = 0
         fun getNewId() = curId++
     }
 
     fun addWire(path: String) {
-        val wireId = getNewId()
+        val wireId = getNewId().toString()
+        var steps = 0
+
         val moves = path.split(",")
         var coords = arrayOf(0,0)
         for (move in moves) {
             val dir = move[0]
             val amount = move.substring(1).toInt()
             for(i in 1..amount) {
+                steps++
                 when(dir) {
                     'U' -> coords[1] += 1
                     'D' -> coords[1] -= 1
@@ -36,11 +40,12 @@ class WireGrid() {
 
                 val key = coords.joinToString(",")
                 val mapPos = map[key]
-                if(mapPos != null && mapPos != wireId.toString() && mapPos != "X") {
-                    map[key] = "X"
+                if(mapPos != null && mapPos[0] != wireId && mapPos[0] != "X") {
+                    map[key] = arrayOf("X", "X")
                     intersections.add(coords.clone())
-                } else {
-                    map[key] = wireId.toString()
+                    intersectionSteps.add(arrayOf(mapPos[1].toInt(), steps))
+                } else if(mapPos == null) {
+                    map[key] = arrayOf(wireId, steps.toString())
                 }
             }
         }
@@ -49,5 +54,6 @@ class WireGrid() {
 
     fun closestDistanceToIntersection() = intersections.map { it.map { it.absoluteValue }.sum() }.min()
 
+    fun minimumStepsIntersection() = intersectionSteps.map { it.sum() }.min()
 }
 
