84f6192fd2125011048c009946ee747336b6678c
Petter Sæther Moen <pettersmoen@gmail.com>
2019-12-08 21:29:22 +0100

    Finished day 8

--- 2019/src/Day8.kt
+++ 2019/src/Day8.kt
@@ -1,4 +1,7 @@
+import java.awt.Color
+import java.awt.image.BufferedImage
 import java.io.File
+import javax.imageio.ImageIO
 
 fun main() {
     val input = File("in/day8.txt").readText().trim().toCharArray().map { it.toString().toInt() }
@@ -8,4 +11,9 @@ fun main() {
     val fewestZeroLayer = layerInputs.sortedBy { layer -> layer.count { it == 0 } }[0]
     println(fewestZeroLayer.count {it == 1} * fewestZeroLayer.count {it == 2})
 
+    val combinedLayers = layerInputs.reduce {acc, cur -> acc.zip(cur).map { (a, b) -> if(a == 2) b else a }}
+    val image = BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR)
+    val colors = arrayOf(Color(0,0,0), Color(255,255,255), Color(255, 255, 255, 255))
+    combinedLayers.forEachIndexed { index, color -> image.setRGB(index % width, index / width, colors[color].rgb) }
+    ImageIO.write(image, "png", File("out/day8.png"))
 }
