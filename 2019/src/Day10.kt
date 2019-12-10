60e7a0f073ab4fcd22c51a7c82ba0693736ceba4
Petter Sæther Moen <pettersmoen@gmail.com>
2019-12-10 15:37:10 +0100

    Solved day 10 problem 2

--- 2019/src/Day10.kt
+++ 2019/src/Day10.kt
@@ -1,48 +1,26 @@
 import java.io.File
-import kotlin.math.acos
-import kotlin.math.atan2
-import kotlin.math.pow
-import kotlin.math.sqrt
+import kotlin.math.*
 
 fun main() {
     val input = File("in/day10.txt").readLines()
-    //val input = listOf(
-    //    "......#.#.",
-    //    "#..#.#....",
-    //    "..#######.",
-    //    ".#.#.###..",
-    //    ".#..#.....",
-    //    "..#....#.#",
-    //    "#..#....#.",
-    //    ".##.#..###",
-    //    "##...#..#.",
-    //    ".#....####"
-    //)
-
     val coordMap = input.mapIndexed { y, row -> row.mapIndexedNotNull { x, c -> if (c == '#') listOf(x, y).map { it.toDouble() } else null } }
     val coords = coordMap.flatten()
     val asteroids = coords.map { Asteroid(it) }
     val asteroidViewCount = asteroids.map { it.detectAsteroids(coords) }
-    println("Most detected: ${asteroidViewCount.max()}")
-    println(angleBetween(listOf(0.0,0.0), listOf(0.0, -1.0)))
-
+    val maxAsteroidCount = asteroidViewCount.max()
+    val asteroidMaxCount = asteroids[asteroidViewCount.indexOf(maxAsteroidCount)]
+    val shootOrder = asteroidMaxCount.asteroidShootOrder(coords)
+    println("Most detected $maxAsteroidCount at position ${asteroidMaxCount.position}")
+    println("The 200th shot asteroid is: ${shootOrder[199]}")
+    println(shootOrder)
 }
-
-
 
 fun angleBetween(a: List<Double>, b: List<Double>): Double {
     val (x, y)  = vecMinus(b, a)
-    return atan2(y, x)
+    return atan2(x, -y)
 }
 
-fun dotProduct(a: List<Double>, b: List<Double>) = a.zip(b).fold(0.0, { acc, (u, v)-> acc + u * v })
-
-fun magnitude(a: List<Double>) = sqrt(a.fold(0.0, {acc, i -> acc + i.toDouble().pow(2)  }))
-
-fun normalize(a: List<Double>): List<Double> {
-    val len = magnitude(a)
-    return a.map { it / len }
-}
+fun magnitude(a: List<Double>) = sqrt(a.fold(0.0, {acc, i -> acc + i.pow(2)  }))
 
 fun vecMinus(a: List<Double>, b: List<Double>) : List<Double> {
     return a.zip(b).map {it.first - it.second}
@@ -52,7 +30,22 @@ class Asteroid(val position: List<Double>) {
     fun detectAsteroids(asteroidMap: List<List<Double>>) =
         asteroidMap
             .filter { it != position }
-            .map { angleBetween(it, position) }
+            .map { angleBetween(position, it) }
             .toSet()
             .size
+
+    fun asteroidShootOrder(asteroidMap: List<List<Double>>) = asteroidMap
+        .asSequence()
+        .filter { it != position }
+        .map { angleBetween(position, it) to it }
+        .groupBy { it.first }
+        .map { group ->
+            val first = (group.key + 2 * PI) % (2 * PI)
+            val second = group.value.map { it.second }.sortedBy { magnitude(vecMinus(it, position)) }[0]
+            first to second
+        }
+        .sortedBy { it.first }
+        .map { it.second }
+        .toList()
 }
+
