957c7c3b58a6bdd72b8892da117bf5afa6e65127
Petter Sæther Moen <pettersmoen@gmail.com>
2019-12-14 12:04:01 +0100

    day 14 problem 1

---
+++ 2019/src/main/java/Day14.kt
@@ -0,0 +1,47 @@
+import java.io.File
+
+typealias Quantity = Int
+typealias Recipe = Pair<Quantity, List<Pair<Quantity, Chemical>>>
+
+fun main() {
+    val pattern = Regex("(\\d+ [A-Z]+)")
+    val input = File("in/day14.txt").readLines()
+
+    val chemicalMap = mutableMapOf<String, Chemical>()
+    for (line in input) {
+        val reaction = pattern.findAll(line)
+            .map { it.value.split(" ") }
+            .map { Pair(it[0].toInt(), chemicalMap.getOrPut(it[1], { Chemical(it[1], it[1] == "ORE") })) }
+            .toList()
+        val chem = reaction.last().second
+        chem.recipe = Pair(reaction.last().first, reaction.dropLast(1))
+    }
+
+    val fuel = chemicalMap["FUEL"]
+    println(fuel?.produce(1))
+
+}
+
+class Chemical(val name: String, private val isOre: Boolean = false) {
+    var recipe: Recipe? = null
+    var excess: Quantity = 0
+
+
+    /**
+     * Returns ore needed
+     */
+    fun produce(quantity: Quantity) : Quantity {
+        if (isOre) return quantity
+        if (recipe == null) error("Missing recipe for $name")
+
+        var coalProduced = 0
+        while (excess < quantity) {
+            coalProduced += recipe!!.second.map { (amountNeeded, chemical) -> chemical.produce(amountNeeded) }.sum()
+            excess += recipe!!.first
+        }
+
+        excess -= quantity
+        return coalProduced
+    }
+
+}
