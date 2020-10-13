bb1655e1fbdb7bb802510f7441d26c97eabca6fb
Petter Sæther Moen <pettersmoen@gmail.com>
2020-10-13 16:30:22 +0200

    day 2 part 2

--- 2015/day02/Day02.hs
+++ 2015/day02/Day02.hs
@@ -1,5 +1,6 @@
 module Day01 where
 
+import Data.List
 import Data.List.Split (splitOn)
 
 parse :: String -> [[Int]]
@@ -19,11 +20,21 @@ boxSlack = minimum . calcSides
 calcNeededWrappingPaper :: [Int] -> Int
 calcNeededWrappingPaper dimensions = (boxSurfaceArea dimensions) + (boxSlack dimensions)
 
-calcBoxes :: [[Int]] -> Int
-calcBoxes = sum . map calcNeededWrappingPaper
+part1 :: [[Int]] -> Int
+part1 = sum . map calcNeededWrappingPaper
+
+calcNeededRibbon :: [Int] -> Int
+calcNeededRibbon dimensions = (ribbon dimensions) + (bow dimensions)
+  where
+    ribbon = (2 *) . sum . take 2 . sort
+    bow = product
+
+part2 :: [[Int]] -> Int
+part2 = sum . map calcNeededRibbon
 
 main :: IO ()
 main = do
   contents <- readFile "input.txt"
   boxes <- return $ parse contents
-  putStrLn $ show $ calcBoxes boxes
+  print $ part1 boxes
+  print $ part2 boxes
