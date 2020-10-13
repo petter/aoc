31ffa1dd4ff64e899cba4a90636e496fbdc5e9bf
Petter Sæther Moen <pettersmoen@gmail.com>
2020-10-13 14:47:08 +0200

    day 1 part 2

--- 2015/day01/Day01.hs
+++ 2015/day01/Day01.hs
@@ -1,14 +1,26 @@
 module Day01 where
 
+parseFloor :: Char -> Int -> Int
+parseFloor '(' n = n + 1
+parseFloor ')' n = n - 1
+
 floorParserHelper :: [Char] -> Int -> Int
-floorParserHelper ('(' : cs) n = floorParserHelper cs (n + 1)
-floorParserHelper (')' : cs) n = floorParserHelper cs (n - 1)
+floorParserHelper (c : cs) n = floorParserHelper cs $ parseFloor c n
 floorParserHelper [] n = n
 
 floorParser :: [Char] -> Int
 floorParser s = floorParserHelper s 0
 
+floorParser2Helper :: [Char] -> Int -> Int -> Int
+floorParser2Helper _ (-1) charPos = charPos
+floorParser2Helper [] _ _ = -1
+floorParser2Helper (c : cs) n charPos = floorParser2Helper cs (parseFloor c n) (charPos + 1)
+
+floorParser2 :: [Char] -> Int
+floorParser2 s = floorParser2Helper s 0 0
+
 main :: IO ()
 main = do
   contents <- readFile "day01.txt"
   putStrLn $ show $ floorParser contents
+  putStrLn $ show $ floorParser2 contents
