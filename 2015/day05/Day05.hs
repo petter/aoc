655fbb542f9bc37ddd31203656bf9379c3a46315
Petter Sæther Moen <pettersmoen@gmail.com>
2020-10-15 15:17:50 +0200

    Day 5 part 2

--- 2015/day05/Day05.hs
+++ 2015/day05/Day05.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.List (isInfixOf)
 import Text.Regex.TDFA
 import Text.Regex.TDFA.Text ()
 
@@ -14,13 +15,32 @@ containsDoubleLetter = any (uncurry (==)) . (zip <*> tail)
 notContainsUnwantedString :: String -> Bool
 notContainsUnwantedString = not . (=~ "ab|cd|pq|xy")
 
-isNiceString :: String -> Bool
-isNiceString s = all id $ fmap ($ s) [containsTwoVowels, containsDoubleLetter, notContainsUnwantedString]
+isNiceString :: [String -> Bool] -> String -> Bool
+isNiceString rules s = all id $ fmap ($ s) rules
 
 part1 :: String -> Int
-part1 = length . filter isNiceString . lines
+part1 = length . filter (isNiceString [containsTwoVowels, containsDoubleLetter, notContainsUnwantedString]) . lines
+
+containsReoccuringPair :: String -> Bool
+containsReoccuringPair [] = False
+containsReoccuringPair (_ : []) = False
+containsReoccuringPair (c1 : c2 : cs) = case [c1, c2] `isInfixOf` cs of
+  True -> True
+  False -> containsReoccuringPair (c2 : cs)
+
+containsReoccuringLetter :: String -> Bool
+containsReoccuringLetter [] = False
+containsReoccuringLetter (_ : []) = False
+containsReoccuringLetter (_ : _ : []) = False
+containsReoccuringLetter (c1 : c2 : c3 : cs) = case c1 == c3 of
+  True -> True
+  False -> containsReoccuringLetter (c2 : c3 : cs)
+
+part2 :: String -> Int
+part2 = length . filter (isNiceString [containsReoccuringPair, containsReoccuringLetter]) . lines
 
 main :: IO ()
 main = do
   contents <- readFile "input.txt"
   print $ part1 contents
+  print $ part2 contents
