7afe8a0399ce2fda9541f722648718d660424ed9
Petter Sæther Moen <pettersmoen@gmail.com>
2020-12-01 08:55:40 +0100

    day 1 part 2

--- 2020/day01/day01.hs
+++ 2020/day01/day01.hs
@@ -3,19 +3,29 @@ module Main where
 
 import Data.List (find)
 
-part1 :: [Int] -> Maybe Int
-part1 input = 
+part1and2 :: [[Int]] -> Maybe Int
+part1and2 input = 
     prodMaybePair sumPair
     where
-        prodMaybePair = fmap (uncurry (*))
-        sumPair = find (sumEqual2020) $ cartProd input input
-        sumEqual2020 p = ((uncurry (+)) p) == 2020
-        cartProd xs ys = [(x,y) | x <- xs, y <- ys]
+        prodMaybePair = fmap product 
+        sumPair = find (sumEqual2020) input
+        sumEqual2020 p = (sum p) == 2020
+
+part1 :: [Int] -> Maybe Int
+part1 input = 
+    part1and2 $ cartProd input input
+    where
+        cartProd xs ys = [[x,y] | x <- xs, y <- ys]
         
-    
+part2 :: [Int] -> Maybe Int
+part2 input = 
+    part1and2 $ cartProd input input input
+    where
+        cartProd xs ys zs = [[x,y,z] | x <- xs, y <- ys, z <- zs]
 
 main :: IO ()
 main = do
     content <- readFile "input.txt"
     let input :: [Int] = fmap read $ lines content 
     print $ part1 input
+    print $ part2 input
