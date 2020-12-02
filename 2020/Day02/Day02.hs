11e779c09b43aa7a4770487c7fcaaa695d88dd3b
Petter Sæther Moen <pettersmoen@gmail.com>
2020-12-02 08:47:56 +0100

    part 2 day 2

--- 2020/Day02/Day02.hs
+++ 2020/Day02/Day02.hs
@@ -29,17 +29,28 @@ parseFile =
             pass <- many1 alphaNum
             return (minN, maxN, c, pass)
 
+countValidPasswords :: ((Int, Int, Char, String) -> Bool) -> [(Int, Int, Char, String)] -> Int
+countValidPasswords validator = length . filter id . fmap validator
+
 part1 :: [(Int, Int, Char, String)] -> Int
-part1 = length . filter id . fmap validatePassword
+part1 = countValidPasswords validatePassword
     where
         validatePassword (minN, maxN, c, pass) = cCount <= maxN && cCount >= minN
             where 
                 cCount = length $ filter (== c) pass
+
+part2 :: [(Int, Int, Char, String)] -> Int
+part2 = countValidPasswords validatePassword
+    where
+        validatePassword (i1, i2, c, pass) = ((pass !! (i1 - 1)) == c) `xor` ((pass !! (i2 - 1)) == c)
+            where xor a b = (a || b) && (not (a && b))
 
 main :: IO ()
 main = do
     file <- readFile "input.txt"
     let passwords = parseFile file 
     print $ part1 passwords
+    print $ part2 passwords
+    
     
 
