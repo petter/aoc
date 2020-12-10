a32422a63445ff806afff067f7fe9eec9c441375
Petter Sæther Moen <pettersmoen@gmail.com>
2020-12-10 14:32:17 +0100

    Day10 part 2

--- 2020/Day10/Day10.hs
+++ 2020/Day10/Day10.hs
@@ -6,14 +6,42 @@ count :: Eq a => [a] -> a -> Int
 count xs x = length $ filter (x ==) xs
 
 part1 :: [Int] -> Int
-part1 nums = (1 + count diffs 1) * (1 + count diffs 3)
+part1 nums = count diffs 1 * count diffs 3
     where
-        sortedNums = sort nums
         zipWithNext = zip <*> tail
-        diffs = uncurry subtract <$> zipWithNext sortedNums
+        diffs = uncurry subtract <$> zipWithNext nums
+
+isValid nums = all (<= 3) diffs
+    where
+        zipWithNext = zip <*> tail
+        diffs = uncurry subtract <$> zipWithNext nums
+
+
+part2 (first:nums) = product ((\(chunkHead:chunkRest) -> combinations [chunkHead] chunkRest) <$> chunks)
+    where 
+        combinations _ [] = 1
+        combinations before (n:[]) = if isValid (before ++ [n]) then 1 else 0
+        combinations before (n:after) = 
+            if isValid (before ++ (n:after)) then 
+                combinations before after + combinations (before ++ [n]) after
+            else 
+                0
+        chunks = chunk [] [first] nums
+        chunk result cur (n:[]) = result ++ [(cur ++ [n])]
+        chunk result cur (n:ns) = 
+            if (n - last cur) == 3 then
+                chunk (result ++ [cur]) [n] ns
+            else 
+                chunk result (cur ++ [n]) ns
 
 main :: IO ()
 main = do
-    file <- readFile "input.txt"
-    let input = read <$> lines file
-    print $ part1 input
+    let fileNames = ["simpletest.txt", "testinput.txt", "input.txt"]
+    files <- traverse readFile fileNames
+    let inputs = zip fileNames $ (sort . (\nums -> nums ++ [0, maximum nums + 3]) . (fmap read) . lines) <$> files
+
+    print "Part 1"
+    print $ (\(a, b) -> (a, part1 b)) <$> inputs
+
+    print "Part 2"
+    print $ (\(a, b) -> (a, part2 b)) <$> inputs
