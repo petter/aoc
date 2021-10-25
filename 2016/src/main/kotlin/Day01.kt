ce488e21cabd9eb433e700cb39713e246cfcd73a
Petter Sæther Moen <pettersmoen@gmail.com>
2021-10-25 23:34:45 +0200

    Bad solve of part2

--- 2016/src/main/kotlin/Day01.kt
+++ 2016/src/main/kotlin/Day01.kt
@@ -1,4 +1,5 @@
 import Direction.*
+import kotlin.math.abs
 
 enum class Direction {
     NORTH, SOUTH, WEST, EAST
@@ -8,12 +9,24 @@ private operator fun Pair<Int, Int>.plus(other : Pair<Int, Int>) = Pair(this.fir
 
 class Walker(var pos : Pair<Int, Int> = Pair(0, 0)) {
     var facing : Direction = NORTH;
+    val visitedLocations : MutableList<Pair<Int, Int>> = mutableListOf(Pair(0, 0))
 
     fun walk(command: String) {
         val facing = command[0]
         val length = command.substring(1).toInt()
         changeFacing(facing)
         move(length)
+    }
+
+    fun walk2(command: String) {
+        val facing = command[0]
+        val length = command.substring(1).toInt()
+        changeFacing(facing)
+        for (i in 1..length) {
+            move(1)
+            visitedLocations.add(pos)
+        }
+
     }
 
     private fun changeFacing(c: Char) {
@@ -58,13 +71,28 @@ class Day01 {
     fun solve() {
         val moves = input.trim().split(", ")
         part1(moves)
+        part2(moves)
     }
 
     fun part1(moves: List<String>) {
+        println("\n--------PART 1--------")
         val walker = Walker()
         moves.forEach(walker::walk)
         println("Position: ${walker.pos}, facing: ${walker.facing}")
-        println("Answer is ${walker.pos.first + walker.pos.second}")
+        println("Answer is ${abs(walker.pos.first) + abs(walker.pos.second)}")
+    }
+
+    fun part2(moves: List<String>) {
+        println("\n--------PART 2--------")
+        val walker = Walker()
+        moves.forEach(walker::walk2)
+
+        walker.visitedLocations.groupBy { it }.forEach { (pos, list) ->
+            if (list.size > 1) {
+                println("Position: ${pos}")
+                println("Answer is ${abs(pos.first) + abs(pos.second)}")
+            }
+        }
     }
 }
 
