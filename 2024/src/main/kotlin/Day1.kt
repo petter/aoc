07bfc286b842497bcef535e394e7dd45f72d3595
Petter Sæther Moen <pettersmoen@gmail.com>
2024-12-01 06:11:58 +0100

    2024 day 1 part 1

--- 2024/src/main/kotlin/Day1.kt
+++ 2024/src/main/kotlin/Day1.kt
@@ -1,7 +1,29 @@
+import kotlin.math.abs
+
 class Day1 : Day {
 
+    fun parseInput(input: List<String>): Pair<List<Int>, List<Int>> =
+        input.map { it.split("   ")
+            .map { num -> num.toInt() }.toPair() }
+            .unzip()
+
     override fun part1(input: List<String>): String {
-        return ""
+        val (list1, list2) = parseInput(input)
+        return totalDistance(list1, list2, 0).toString()
+    }
+
+    fun totalDistance(list1: List<Int>, list2: List<Int>, sum: Int): Int {
+        if(list1.isEmpty()) return sum
+
+        val smallest1 = list1.minOrNull()!!
+        val newList1 = list1.toMutableList()
+        newList1.remove(smallest1)
+
+        val smallest2 = list2.minOrNull()!!
+        val newList2 = list2.toMutableList()
+        newList2.remove(smallest2)
+
+        return totalDistance(newList1, newList2, sum + abs(smallest1 - smallest2))
     }
 
     override fun part2(input: List<String>): String {
