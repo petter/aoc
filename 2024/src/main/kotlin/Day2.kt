8e3e172a6e2165953a88b6a3373e8f5abcd8a733
Petter Sæther Moen <pettersmoen@gmail.com>
2024-12-02 08:10:04 +0100

    2024 day 2 part 1

--- 2024/src/main/kotlin/Day2.kt
+++ 2024/src/main/kotlin/Day2.kt
@@ -1,8 +1,29 @@
+import kotlin.math.abs
+
 class Day2 : Day {
 
-    override fun part1(input: List<String>): String {
-        return ""
+    private fun parseInput(input: List<String>): List<List<Int>> {
+        return input.map { it.split(" ").map { num -> num.toInt() } }
     }
+
+    override fun part1(input: List<String>): String {
+        val reports = parseInput(input)
+        val safeReports = reports.filter { isSafe(it) }
+        return safeReports.size.toString()
+    }
+
+    private fun isSafe(report: List<Int>): Boolean {
+        val increasingOrDecreaseing = isIncreasing(report) || isDecreasing(report)
+        val smallAdjacents = isSmallAdjacents(report)
+
+        return increasingOrDecreaseing && smallAdjacents
+    }
+
+    private fun isIncreasing(report: List<Int>) = report.zipWithNext().all { (a, b) -> a < b }
+    private fun isDecreasing(report: List<Int>) = report.zipWithNext().all { (a, b) -> a > b }
+
+    private fun isSmallAdjacents(report: List<Int>) = report.zipWithNext().all { (a, b) -> abs(a - b) in 1..3 }
+
 
     override fun part2(input: List<String>): String {
         return ""
--- 2024/src/main/kotlin/Main.kt
+++ 2024/src/main/kotlin/Main.kt
@@ -120,7 +120,7 @@ fun runAll() {
 
 val solutions = mapOf(
     1 to Day1(),
-//    2 to Day2(),
+    2 to Day2(),
 //    3 to Day3(),
 //    4 to Day4(),
 //    5 to Day5(),
@@ -148,7 +148,7 @@ val solutions = mapOf(
 
 val expectedResults = mapOf(
     1 to Pair("2031679", "19678534"),
-//    2 to Pair("0", "0"),
+    2 to Pair("0", "0"),
 //    3 to Pair("0", "0"),
 //    4 to Pair("0", "0"),
 //    5 to Pair("0", "0"),
