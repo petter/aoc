7ba0288f5f78ab91d459c92edc97adb0ecbf157e
Petter Sæther Moen <pettersmoen@gmail.com>
2024-12-03 07:38:15 +0100

    2024 day 3 part 2

--- 2024/src/main/kotlin/Day3.kt
+++ 2024/src/main/kotlin/Day3.kt
@@ -1,18 +1,37 @@
 class Day3 : Day {
-    private fun parseInput(input: List<String>): List<Pair<Int, Int>> {
+    private fun parseInput(input: List<String>, isPart2: Boolean): List<Pair<Int, Int>> {
         val s = input.joinToString { it }
-        val matcher = Regex("mul\\((\\d+),(\\d+)\\)")
+        val matcher = Regex("(mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\))")
         val matches = matcher.findAll(s)
-        return matches.map { it.groupValues.drop(1).map { value -> value.toInt() }.toPair() }.toList()
+
+        var mulEnabled = true
+        return matches.mapNotNull { group ->
+            if (group.groupValues.first() == "do()") {
+                mulEnabled = true
+                return@mapNotNull null
+            }
+            if (group.groupValues.first() == "don't()") {
+                mulEnabled = false
+                return@mapNotNull null
+            }
+            if(!mulEnabled && isPart2) {
+                return@mapNotNull null
+            }
+            group.groupValues.drop(2).map { value ->
+                value.toInt()
+            }.toPair()
+        }.toList()
     }
 
     override fun part1(input: List<String>): String {
-        val multiplications = parseInput(input)
+        val multiplications = parseInput(input, false)
         return multiplications.sumOf { (a, b) -> a * b }.toString()
     }
 
+    // 154365 - too low
     override fun part2(input: List<String>): String {
-        return ""
+        val multiplications = parseInput(input, true)
+        return multiplications.sumOf { (a, b) -> a * b }.toString()
     }
 
 }
--- 2024/src/main/kotlin/Main.kt
+++ 2024/src/main/kotlin/Main.kt
@@ -149,7 +149,7 @@ val solutions = mapOf(
 val expectedResults = mapOf(
     1 to Pair("2031679", "19678534"),
     2 to Pair("680", "710"),
-    3 to Pair("174336360", "0"),
+    3 to Pair("174336360", "88802350"),
 //    4 to Pair("0", "0"),
 //    5 to Pair("0", "0"),
 //    6 to Pair("0", "0"),
