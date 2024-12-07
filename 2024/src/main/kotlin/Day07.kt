5b57535b5e988d330c2ce5e687839a0c8e9102b9
Petter Sæther Moen <pettersmoen@gmail.com>
2024-12-07 17:30:01 +0100

    2024 day 7

--- 2024/src/main/kotlin/Day07.kt
+++ 2024/src/main/kotlin/Day07.kt
@@ -1,11 +1,58 @@
 class Day07 : Day {
+    private fun parseInput(input: List<String>): List<Pair<Long, List<Long>>> {
+        return input.map { line ->
+            val (total, nums) = line.split(": ")
+            total.toLong() to nums.split(" ").map { it.toLong() }
+        }
+    }
+
+    private fun isTrue(total: Long, nums: List<Long>, operators: List<Operator>): Boolean {
+        if(nums.size == 1) return nums.first() == total
+        if(nums.first() > total || nums.isEmpty()) return false
+
+        val nextNums = nums.take(2)
+        val rest = nums.drop(2)
+        return operators.any { op ->
+            isTrue(
+                total,
+                listOf(op.apply(nextNums[0], nextNums[1])) + rest,
+                operators
+            )
+        }
+    }
 
     override fun part1(input: List<String>): String {
-        return ""
+        val equations = parseInput(input)
+        val operators = listOf(Operator.Add, Operator.Mul)
+        val trueEquations = equations.filter { (total, nums) -> isTrue(total, nums, operators) }
+        return trueEquations.sumOf { it.first }.toString()
     }
+
 
     override fun part2(input: List<String>): String {
-        return ""
+        val equations = parseInput(input)
+        val operators = listOf(Operator.Add, Operator.Mul, Operator.Mul, Operator.Concat)
+        val trueEquations = equations.filter { (total, nums) -> isTrue(total, nums, operators) }
+        return trueEquations.sumOf { it.first }.toString()
     }
+}
 
+sealed class Operator {
+    abstract fun apply(a: Long, b: Long): Long
+
+    data object Add : Operator() {
+        override fun apply(a: Long, b: Long): Long {
+            return a + b
+        }
+    }
+    data object Mul : Operator() {
+        override fun apply(a: Long, b: Long): Long {
+            return a * b
+        }
+    }
+    data object Concat : Operator() {
+        override fun apply(a: Long, b: Long): Long {
+            return "$a$b".toLong()
+        }
+    }
 }
